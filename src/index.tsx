af174f6d8acc8a64c2c1773a5f0ca201f8eede3e
surekhag <surekha19.mca@gmail.com>
2023-03-12 22:21:08 +0530

    Updated configs as import

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import configData from "configData"
+import configs from "configData"
 const App = () => {
-  console.log("configData",configData)
+  console.log("configData",configs)
   return (
   <h1>My React and TypeScript App along with Webpack 5!</h1>
   )
